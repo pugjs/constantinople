109dd3e1e94fd803c084008a898b911655bbc243
ForbesLindesay <forbes@lindesay.co.uk>
2013-06-26 04:40:54 +0100

    Initial Code

---
+++ test/index.js
@@ -0,0 +1,45 @@
+'use strict'
+
+var assert = require('assert')
+var constaninople = require('../')
+
+describe('isConstant(src)', function () {
+  it('handles "[5 + 3 + 10]"', function () {
+    assert(constaninople.isConstant('[5 + 3 + 10]') === true)
+  })
+  it('handles "/[a-z]/.test(\'a\')"', function () {
+    assert(constaninople.isConstant('/[a-z]/.test(\'a\')') === true)
+  })
+  it('handles "Math.random()"', function () {
+    assert(constaninople.isConstant('Math.random()') === false)
+  })
+  it('handles "Math.random("', function () {
+    assert(constaninople.isConstant('Math.random(') === false)
+  })
+})
+
+
+describe('toConstant(src)', function () {
+  it('handles "[5 + 3 + 10]"', function () {
+    assert.deepEqual(constaninople.toConstant('[5 + 3 + 10]'), [5 + 3 + 10])
+  })
+  it('handles "/[a-z]/.test(\'a\')"', function () {
+    assert(constaninople.toConstant('/[a-z]/.test(\'a\')') === true)
+  })
+  it('handles "Math.random()"', function () {
+    try {
+      constaninople.toConstant('Math.random()')
+    } catch (ex) {
+      return
+    }
+    assert(false, 'Math.random() should result in an error')
+  })
+  it('handles "Math.random("', function () {
+    try {
+      constaninople.toConstant('Math.random(')
+    } catch (ex) {
+      return
+    }
+    assert(false, 'Math.random( should result in an error')
+  })
+})
